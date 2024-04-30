f66c982eec6b27fe0208510ac6711187869b8086
anangfirmansyah <anangfirmansyahp5@gmail.com>
2024-04-30 10:26:01 +0800

    fix: schema

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import authRouter from "./auth-routes";
 import customerRouter from "./customer-routes";
-import eventGuestSeatRouter from "./event-guest-seat-routes";
-import eventPaymentRouter from "./event-payment-router";
 import eventRouter from "./event/event";
 import eventRundownRouter from "./event-rundown-routes";
 import { eventVendorRouter } from "./event-vendor-routes";
@@ -11,8 +9,6 @@ import { vendorRouter } from "./vendor-routes";
 import { roleRoutes } from "./role.router";
 import permissionRouter from "./permission.router";
 import vendorNoteRouter from "./vendor-note.router";
-import checklistHolmatRouter from "./checklist-holmat.router";
-import checklistHolmatItemRouter from "./checklist-holmat-item.router";
 import collaborator from "./event/collaborator";
 import biodata from "./event/biodata";
 
@@ -25,11 +21,7 @@ rootRouter.use("/vendors", [vendorRouter, vendorNoteRouter]);
 rootRouter.use("/events", [
   eventRouter,
   eventRundownRouter,
-  eventPaymentRouter,
-  eventGuestSeatRouter,
   eventVendorRouter,
-  checklistHolmatRouter,
-  checklistHolmatItemRouter,
   collaborator,
   biodata,
 ]);
