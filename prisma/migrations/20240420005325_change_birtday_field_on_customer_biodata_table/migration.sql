5002bda7c2e2b657b9a8cdf64fff2249f6d25011
anangfirmansyah <anangfirmansyahp5@gmail.com>
2024-05-01 15:34:35 +0800

    fix: migration

--- prisma/migrations/20240420005325_change_birtday_field_on_customer_biodata_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `customer_biodatas` MODIFY `birthday` DATETIME(3) NULL;
--- prisma/migrations/20240420010738_add_cascade_delete_on_customer_biodata/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `customer_biodatas` DROP FOREIGN KEY `customer_biodatas_customer_id_fkey`;
-
--- AddForeignKey
-ALTER TABLE `customer_biodatas` ADD CONSTRAINT `customer_biodatas_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
